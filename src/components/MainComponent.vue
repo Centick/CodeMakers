df54b42d6e250c1c2f127e9fd4a888e4ba58b609
Centick <utyatsky.ry@yandex.ru>
2024-11-19 00:58:44 +0300

    vue + banner 40% ✅

---
+++ src/components/MainComponent.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="section header__section">
+        <div class="container header__container">
+            <nav class="header_nav">
+                <div>
+                    <a class="link header_link header_link--active" href="№">Home</a>
+                    <a class="link header_link " href="№">О нас</a>
+                    <a class="link header_link" href="№">Резюме</a>
+                </div>
+                <a class="link header_link--logo" href="#">CodeMakers</a>
+                <div>
+                    <a class="link header_link" href="#">Еще что-то</a>
+                    <a class="link header_link" href="№">Проекты</a>
+                    <a class="link header_link" href="№">Контакты</a>
+                </div>
+            </nav>
+        </div>
+    </div>
+
+    <div class="section banner__section">
+        <div class="container banner__container">
+            <h1>CODEMAKERS</h1>
+            
+        </div>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style> 
+
+</style>
